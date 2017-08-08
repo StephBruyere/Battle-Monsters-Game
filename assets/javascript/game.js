f7d3db18b976560704e823ba1a4a541171ccc4cb
StephBruyere <stephbruyeredesigns@gmail.com>
2017-08-07 23:03:52 -0400

    third commit for debuggin

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,11 +9,17 @@
 
 //globals
 /*
-
-var active = avatar, computer choice;
-var avatar = 
-var enemy = 
+ 
 */
+var monsterName = $("#voodoo, #toxic, #easter, #screamer, #invman, #hair, #fish");
+var avatar; 
+var setChar = false;
+var userSelection;
+var enemy;
+var computerEnemy = false;
+
+//=======================================================
+
 var voodooMonster = new monster("Voodoo", 125,50,10,true)
 var easterMonster = new monster("Voodoo", 100,75,10,true)
 var toxicMonster = new monster("Voodoo", 150,25,10,true)
@@ -23,95 +29,148 @@ var hairMonster = new monster("Voodoo", 175,10,10,true)
 var fishMonster = new monster("Voodoo", 100,25,25,true)
 
 
-//=======================================================
-
-//onload set music to on/ create button to set music to off
-
 function monster(name, health, attack, counter, alive) {
     this.name = name;
     this.health = health;
-    this.attack = attack;
-    this.counter = counter;
-    this.alive = alive;
-}
-
-function avatarMonster(name, health, attack, alive) {
-    this.name = name;
-    this.health = health;
     this.attack = attack += 10;
-    this.alive = alive;
-}
-
-function enemyMonster(name, health, counter, alive) {
-    this.name = name;
-    this.health = health;
     this.counter = counter;
     this.alive = alive;
 }
+
+
+// Setting User Selection and Enemy Selection =======================================================
+
+
+         if (setChar == false) {
+          $('#voodoo').on("click", function() {
+              var avatar = $("#voodoo");
+              setChar = true;
+              console.log(avatar);
+              console.log(setChar);
+          });
+      } 
+
+      else if (setChar == true) {
+          $('#voodoo').on("click", function() {
+              if (computerEnemy == false) {
+                  chooseOpponent(voodoo);
+                  console.log(setChar+"1");
+              }
+          });
+
+          function chooseOpponent(voodoo) {
+              if (computerEnemy == false) {
+                  var enemy = voodoo;
+                  computerEnemy = true;
+                  console.log(enemy);
+              }
+          }
+      } else {
+
+      }
+
+// Toxic  =======================================================
+
+      if (setChar == false) {
+          $('#toxic').on("click", function() {
+              var avatar = $("#toxic");
+              setChar = true;
+              console.log(avatar);
+               console.log(setChar+"2");
+          });
+       }
+      else if (setChar == true) {
+          $('#toxic').on("click", function() {
+              if (computerEnemy == false) {
+                  chooseOpponent(toxic);
+                   console.log(setChar+"3");
+              }
+          });
+
+          function chooseOpponent(toxic) {
+              if (computerEnemy == false) {
+                  var enemy = toxic;
+                  computerEnemy = true;
+                  console.log(enemy);
+              }
+          }
+      } else {
+
+      }
+
+//function enemyMonster (enemy) {
+//  var life = (var i = this.health; i >= 0 , counter)
+//
 
 
 
 // make if statement later for positioning. If voodoo and toxic (x,y)
 
+function animations () {
+
       $("#voodoo").on("click", function() { 
         $("#voodoo").animate({ top: 10});
-        $("#voodoo").animate({ height: "350px"},1200);
-        $(".voodoo2").animate({ width: "125px"},1200);
-        $("#voodoo").animate({ left: 200});
+        $("#voodoo").animate({ height: "325px"},1200);
+        $(".voodoo2").animate({ width: "55px"},1200);
+        $("#voodoo").animate({ left: 0});
       });
 
       $("#toxic").on("click", function() { 
         $("#toxic").animate({ top: 10});
         $("#toxic").animate({ height: "325px"},1200);
-        $(".toxic2").animate({ width: "150px"},1200);
-        $("#toxic").animate({ left: 100});
+        $(".toxic2").animate({ width: "55px"},1200);
+        $("#toxic").animate({ left: 0});
       });
 
         $("#easter").on("click", function() { 
         $("#easter").animate({ top: 10});
         $("#easter").animate({ height: "325px"},1200);
-        $(".easter2").animate({ width: "150px"},1200);
-        $("#easter").animate({ left: 150});
+        $(".easter2").animate({ width: "55px"},1200);
+        $("#easter").animate({ left: 0});
       });
 
         $("#screamer").on("click", function() { 
         $("#screamer").animate({ top: 10});
         $("#screamer").animate({ height: "325px"},1200);
-        $(".screamer2").animate({ width: "150px"},1200);
-        $("#screamer").animate({ left: 20});
+        $(".screamer2").animate({ width: "55px"},1200);
+        $("#screamer").animate({ left: 0});
       });
 
         $("#screamer").on("click", function() { 
         $("#screamer").animate({ top: 10});
         $("#screamer").animate({ height: "325px"},1200);
-        $(".screamer2").animate({ width: "200px"},1200);
-        $("#screamer").animate({ left: 20});
+        $(".screamer2").animate({ width: "55px"},1200);
+        $("#screamer").animate({ left: 0});
       });
 
         $("#invman").on("click", function() { 
         $("#invman").animate({ top: 10});
         $("#invman").animate({ height: "325px"},1200);
-        $(".invman2").animate({ width: "150px"},1200);
+        $(".invman2").animate({ width: "55px"},1200);
         $("#invman").animate({ left: 0});
       });
 
         $("#fish").on("click", function() { 
         $("#fish").animate({ top: 10});
         $("#fish").animate({ height: "325px"},1200);
-        $(".fish2").animate({ width: "150px"},1200);
-        $("#fish").animate({ right: 150});
+        $(".fish2").animate({ width: "55px"},1200);
+        $("#fish").animate({ right: 0});
       });
 
         $("#hair").on("click", function() { 
         $("#hair").animate({ top: 10});
         $("#hair").animate({ height: "325px"},1200);
-        $(".hair2").animate({ width: "125px"},1200);
-        $("#hair").animate({ right: 100});
+        $(".hair2").animate({ width: 55px"},1200);
+        $("#hair").animate({ right: 0});
       });
 
+}
 
+animations ();
 
 /*
+
+//onload set music to on/ create button to set music to off
 
 $()
 // onclick set object as avatar
