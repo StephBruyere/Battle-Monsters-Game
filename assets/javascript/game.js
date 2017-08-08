b4df3479be5c5282e21f275799427f2dc1dcf0a6
StephBruyere <stephbruyeredesigns@gmail.com>
2017-08-07 20:36:37 -0400

    replacement files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,36 +13,102 @@
 var active = avatar, computer choice;
 var avatar = 
 var enemy = 
-
-var hero1 = {
-	health: 100,
-    attack: 50,
-    counter: 50,
-    alive: true,
-}
-
-var hero2 = {
-	health: 200,
-    attack: 25,
-    counter: 25,
-    isAlive: true,
-}
-
 */
+var voodooMonster = new monster("Voodoo", 125,50,10,true)
+var easterMonster = new monster("Voodoo", 100,75,10,true)
+var toxicMonster = new monster("Voodoo", 150,25,10,true)
+var screamerMonster = new monster("Voodoo", 1,50,10,true)
+var invmanMonster = new monster("Voodoo", 75,50,20,true)
+var hairMonster = new monster("Voodoo", 175,10,10,true)
+var fishMonster = new monster("Voodoo", 100,25,25,true)
+
 
 //=======================================================
 
 //onload set music to on/ create button to set music to off
 
-      $("#voodoo").on("click", function() {
-        $("#voodoo").animate({ height: "300px"});
-        $(".voodoo2").animate({ width: "100px"});
+function monster(name, health, attack, counter, alive) {
+    this.name = name;
+    this.health = health;
+    this.attack = attack;
+    this.counter = counter;
+    this.alive = alive;
+}
+
+function avatarMonster(name, health, attack, alive) {
+    this.name = name;
+    this.health = health;
+    this.attack = attack += 10;
+    this.alive = alive;
+}
+
+function enemyMonster(name, health, counter, alive) {
+    this.name = name;
+    this.health = health;
+    this.counter = counter;
+    this.alive = alive;
+}
+
+
+
+// make if statement later for positioning. If voodoo and toxic (x,y)
+
+      $("#voodoo").on("click", function() { 
+        $("#voodoo").animate({ top: 10});
+        $("#voodoo").animate({ height: "350px"},1200);
+        $(".voodoo2").animate({ width: "125px"},1200);
+        $("#voodoo").animate({ left: 200});
       });
 
-            $("#donut").on("click", function() {
-        $("#donut").animate({ height: "300px"});
-        $(".donut2").animate({ width: "100px"});
+      $("#toxic").on("click", function() { 
+        $("#toxic").animate({ top: 10});
+        $("#toxic").animate({ height: "325px"},1200);
+        $(".toxic2").animate({ width: "150px"},1200);
+        $("#toxic").animate({ left: 100});
       });
+
+        $("#easter").on("click", function() { 
+        $("#easter").animate({ top: 10});
+        $("#easter").animate({ height: "325px"},1200);
+        $(".easter2").animate({ width: "150px"},1200);
+        $("#easter").animate({ left: 150});
+      });
+
+        $("#screamer").on("click", function() { 
+        $("#screamer").animate({ top: 10});
+        $("#screamer").animate({ height: "325px"},1200);
+        $(".screamer2").animate({ width: "150px"},1200);
+        $("#screamer").animate({ left: 20});
+      });
+
+        $("#screamer").on("click", function() { 
+        $("#screamer").animate({ top: 10});
+        $("#screamer").animate({ height: "325px"},1200);
+        $(".screamer2").animate({ width: "200px"},1200);
+        $("#screamer").animate({ left: 20});
+      });
+
+        $("#invman").on("click", function() { 
+        $("#invman").animate({ top: 10});
+        $("#invman").animate({ height: "325px"},1200);
+        $(".invman2").animate({ width: "150px"},1200);
+        $("#invman").animate({ left: 0});
+      });
+
+        $("#fish").on("click", function() { 
+        $("#fish").animate({ top: 10});
+        $("#fish").animate({ height: "325px"},1200);
+        $(".fish2").animate({ width: "150px"},1200);
+        $("#fish").animate({ right: 150});
+      });
+
+        $("#hair").on("click", function() { 
+        $("#hair").animate({ top: 10});
+        $("#hair").animate({ height: "325px"},1200);
+        $(".hair2").animate({ width: "125px"},1200);
+        $("#hair").animate({ right: 100});
+      });
+
 
 
 /*
